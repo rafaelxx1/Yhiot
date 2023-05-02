95564268e14f43474d1bfbb6130f42acd389917a
rafaelxx1 <rafaelsantosfr@hotmail.com>
2023-05-02 01:29:29 -0300

    migrations

---
+++ Back/src/Yhiot.API/Controllers/AgendamentoController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Yhiot.API.Data;
+using Yhiot.API.Models;
+
+namespace Yhiot.API.Controllers;
+
+[ApiController]
+[Route("api/fracasso")]
+public class EventoController : ControllerBase
+{
+  
+        private readonly DataContext _context;
+
+    public EventoController(DataContext context)
+    {
+            this._context = context;
+
+    }
+
+    [HttpGet]
+    public IEnumerable<Agendamento> Get()
+    {
+        return _context.agendamentos;
+
+    }
+
+    [HttpGet("{id}")]
+    public Agendamento GetById(int id)
+    {
+        return _context.agendamentos.FirstOrDefault(Agendamento => Agendamento.AgendamentoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Post";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return $"Put id = {id}";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return $"Exemplo deletee id = {id}";
+    }
+}
---
+++ Back/src/Yhiot.API/Data/Migrations/20230502042623_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yhiot.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "agendamentos",
+                columns: table => new
+                {
+                    AgendamentoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExameName = table.Column<string>(type: "TEXT", nullable: true),
+                    DataAgendamento = table.Column<string>(type: "TEXT", nullable: true),
+                    Medico = table.Column<string>(type: "TEXT", nullable: true),
+                    cpf = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_agendamentos", x => x.AgendamentoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "agendamentos");
+        }
+    }
+}
