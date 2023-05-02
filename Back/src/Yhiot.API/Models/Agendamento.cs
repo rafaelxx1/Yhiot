a05ea9c66179690471704c896b4b6e2ed9382aa5
rafaelxx1 <rafaelsantosfr@hotmail.com>
2023-05-02 01:17:08 -0300

    Initial Configurations

---
+++ Back/src/Yhiot.API/Models/Agendamento.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yhiot.API.Models
+{
+    public class Agendamento
+    {
+        public int AgendamentoId {get; set; }
+        public string? ExameName {get; set; }
+        public string? DataAgendamento {get; set; }
+        public string? Medico {get; set;}
+        public string? cpf {get; set; }
+    }
+}
